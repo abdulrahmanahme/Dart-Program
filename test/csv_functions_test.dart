7aa9d7cbd741dec307dc7e62427b47df293590a7
abdulrahmanahme <abdelramanahmed00@gmail.com>
2023-05-21 18:04:35 +0200

    Add test

--- test/csv_functions_test.dart
+++ test/csv_functions_test.dart
@@ -1,43 +1,45 @@
+import 'package:path/path.dart' show dirname, join;
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'package:csv_file/csv_functions.dart';
+import 'package:csv/csv.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-void main() {
-  // Arrange
-  group('returnData', () {
-    test('the  function should return the data', () async {
-      // arrange
-      var expectedData = [
-        [
-          'D1',
-          'Minneapolis',
-          'shoes',
-          2,
-          'Air',
-        ],
-        [
-          'ID2',
-          'Chicago',
-          'shoes',
-          1,
-          'Air',
-        ],
-        [
-          'D3',
-          'Central Department Store',
-          'shoes',
-          5,
-          'BonPied',
-        ],
-        [
-          'ID4',
-          'Quail Hollow',
-          'forks',
-          3,
-          'Pfitzcraft',
-        ],
-      ];
 
-      // assert
-      expect(expectedData, equals(expectedData));
-    });
+void main() {
+  // Load the csv file before running the tests
+  setUp(() async {
+    final path = join(dirname(Platform.script.toFilePath()), 'assets/test.csv');
+    final content = await File(path).readAsString();
+    getCsvFile = const CsvToListConverter().convert(content);
+  });
+
+  test('test the firstFile function', () async {
+    // get the expected result
+    final expected = [
+    ['shoes', 2.0], 
+    ['forks', 0.75]
+     
+    ];
+
+    // call the function and get the result
+    final result = await firstFile();
+
+    // compare the result with the expected value
+    expect(result, equals(expected));
+  });
+
+  test('test the secondFile function', () async {
+    // get the expected result
+    final expected = [
+     ['shoes', 'Air\r'], ['forks', 'Pfitzcraft']
+      
+    ];
+
+    // call the function and get the result
+    final result = await secondFile() ;
+
+    // compare the result with the expected value
+    expect(result, equals(expected));
   });
 }
